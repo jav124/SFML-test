6a5bb2500c975dc05a70a4a8e309e505c061df71
Mario Carrillo <mario.carrillo@clearcheck.us>
2024-04-24 10:58:12 -0600

    sandbox v1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 using namespace std;
 
 int width = 400;
-int numCells = 20;
+int numCells = 50;
 int size = width / numCells;
 vector<vector<int>> grid(numCells, vector<int>(numCells, 0));
 vector<vector<int>> nextGrid(numCells, vector<int>(numCells, 0));
@@ -18,10 +18,10 @@ void logGrid();
 int main()
 {
     RenderWindow window(VideoMode(width, width), "SFML works!");
-    window.setFramerateLimit(3);
+    window.setFramerateLimit(45);
     setToZero(grid);
     setToZero(nextGrid);
-    bool play = false;
+    bool play = true;
 
     while (window.isOpen())
     {
@@ -31,13 +31,13 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
 
-            if (event.type == Event::MouseButtonPressed)
+            if (event.type == Event::MouseMoved)
             {
-                if (event.mouseButton.button == Mouse::Left)
+                if (Mouse::isButtonPressed(Mouse::Left))
                 {
-                    int x = event.mouseButton.x / (width / numCells);
-                    int y = event.mouseButton.y / (width / numCells);
-                    grid[y][x] = (grid[y][x] + 1)%2;
+                    int x = event.mouseMove.x / (width / numCells);
+                    int y = event.mouseMove.y / (width / numCells);
+                    grid[y][x] = 1;
                 }
             }
 
@@ -77,8 +77,6 @@ void drawGrid(RenderWindow &window)
 {
     int size = width / numCells;
     RectangleShape rectangle(Vector2f(size, size));
-    rectangle.setOutlineColor(Color::Black);
-    rectangle.setOutlineThickness(1);
     for (int i = 0; i < numCells; i++)
     {
         for (int j = 0; j < numCells; j++)
@@ -103,7 +101,7 @@ void logGrid()
     {
         for (int j = 0; j < numCells; j++)
         {
-            cout << grid[i][j] << " ";
+            cout << grid[j][i] << " ";
         }
         cout << endl;
     }
@@ -117,40 +115,47 @@ void updateGrid()
     {
         for (int j = 0; j < numCells; j++)
         {
-            int neighbors = 0;
-            for (int x = -1; x <= 1; x++)
-            {
-                for (int y = -1; y <= 1; y++)
-                {
-                    if (x == 0 && y == 0)
-                    {
-                        continue;
-                    }
-                    int xIndex = i + x;
-                    int yIndex = j + y;
-                    if (xIndex >= 0 && xIndex < numCells && yIndex >= 0 && yIndex < numCells)
-                    {
-                        if (grid[yIndex][xIndex] == 1)
-                            neighbors++;
-                    }
-                }
-            }
+            if (grid[j][i] == 0)
+                continue;
             if (grid[j][i] == 1)
             {
-                if (neighbors < 2 || neighbors > 3)
+                if (j < numCells - 1 &&
+                    grid[j + 1][i] == 0)
                 {
+                    nextGrid[j + 1][i] = 1;
                     nextGrid[j][i] = 0;
                 }
                 else
                 {
-                    nextGrid[j][i] = 1;
-                }
-            }
-            else
-            {
-                if (neighbors == 3)
-                {
-                    nextGrid[j][i] = 1;
+                    if (j < numCells - 2 && (grid[j + 2][i - 1] == 0 || grid[j + 2][i + 1] == 0))
+                    {
+                        if (rand() % 2 == 0)
+                        {
+                            if (grid[j + 2][i - 1] == 0)
+                            {
+                                nextGrid[j + 2][i - 1] = 1;
+                            }
+                            else if (grid[j + 2][i + 1] == 0)
+                            {
+                                nextGrid[j + 2][i + 1] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if (grid[j + 2][i + 1] == 0)
+                            {
+                                nextGrid[j + 2][i - 1] = 1;
+                            }
+                            else if (grid[j + 2][i - 1] == 0)
+                            {
+                                nextGrid[j + 2][i + 1] = 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        nextGrid[j][i] = 1;
+                    }
                 }
             }
         }
