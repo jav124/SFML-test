a5d925b6dec2010a066845db4eafa9d5e9211050
javier_morfin <19044@portalcedi.edu.mx>
2024-05-08 14:26:58 -0600

    grid

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include "Grid.hpp"
 using namespace sf;
+
+int numCells = 20;
+int width = 700;
+int height = 700;
+
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
-
+    bool play = false;
+    RenderWindow window(VideoMode(width, height), "SFML works!");
+    window.setFramerateLimit(5);
+    Grid grid(numCells, width, height);
     while (window.isOpen())
     {
         Event event;
@@ -13,10 +19,27 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
+
+            if (event.type == Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    int x = event.mouseButton.x;
+                    int y = event.mouseButton.y;
+                    grid.toggle(x, y);
+                }
+                if (event.mouseButton.button == Mouse::Right)
+                {
+                    play = !play;
+                }
+            }
+
         }
 
         window.clear();
-        window.draw(shape);
+        if (play)
+            grid.update();
+        grid.drawTo(window);
         window.display();
     }
 
